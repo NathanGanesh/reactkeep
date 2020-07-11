5ead412a2b9e7a75ad10b32daedfc637bb5c3213
nathanganesh <nathangemieee@gmail.com>
2020-07-11 19:10:14 +0200

    push 2

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>deez nuts</h1>
+        <p>deez nuts gottem ahahah</p>
+    </div>
+}
+
+export default Note;
